b33f78ef1971e2e39313fcf0eb792517d846f22d
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-21 17:05:01 +0200

    Add HomeController and PizzaController with corresponding views and fragments

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/HomeController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/HomeController.java
@@ -1,21 +1,16 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.controller;
 
-import org.lessons.java.spring_la_mia_pizzeria_crud.repo.PizzaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
-@RequestMapping("/pizzas")
-public class PizzaController {
-
+@RequestMapping("/")
+public class HomeController {
     @GetMapping
-    public String pizzasIndex() {
-        return "pizzas/index";
+    public String home() {
+        return "home";
     }
-    
 }
