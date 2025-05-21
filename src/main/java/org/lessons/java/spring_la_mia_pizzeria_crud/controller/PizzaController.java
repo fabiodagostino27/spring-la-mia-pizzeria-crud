59fbe4e93982742e2e3d018a5e9c9ba9d9fd70bf
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-21 18:05:51 +0200

    Add pizza detail view and search functionality; update index page links

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -1,6 +1,7 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repo.PizzaRepository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -25,5 +27,18 @@ public class PizzaController {
         model.addAttribute("pizzas", pizzas);
         return "pizzas/index";
     }
+
+    @GetMapping("/{id}")
+    public String getMethodName(@PathVariable("id") Integer id, Model model) {
+        Pizza pizza = repository.findById(id).get();
+        model.addAttribute(pizza);
+        return "pizzas/show";
+    }
     
+    @GetMapping("/search")
+    public String getMethodName(@RequestParam String name, Model model) {
+        List<Pizza> pizzas = repository.findByNameContaining(name);
+        model.addAttribute("pizzas", pizzas);
+        return "pizzas/index";
+    }
 }
