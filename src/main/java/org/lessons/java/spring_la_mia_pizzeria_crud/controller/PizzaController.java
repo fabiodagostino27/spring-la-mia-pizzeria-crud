197cae1615dc551ceb267e75ab7608fb3940976e
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-26 12:18:48 +0200

    Add delete functionality for pizzas: implement delete method in controller and modal confirmation in show template

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -81,4 +81,12 @@ public class PizzaController {
 
         return "redirect:/pizzas";
     }
+
+    @PostMapping("delete/{id}")
+    public String delete(@PathVariable("id") Integer id) {
+        repository.deleteById(id);
+        
+        return "redirect:/pizzas";
+    }
+    
 }
